19829020a91e17ec655909435e88d8b338a53713
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-10-24 21:25:42 +0800

    host影子根元素

--- collapse.js
+++ collapse.js
@@ -5,7 +5,15 @@ class Collapse extends HTMLElement{
         const tmpl = document.getElementById('collapse_tmpl');
         let cloneTemplate = tmpl.content.cloneNode(true);
         let style = document.createElement('style');
-
+        // :host 代表的是影子的根元素
+        style.textContent = `
+            :host{
+                display:flex;
+                border:3px solid #ebebeb;
+                border-radius:5px;
+                width:100%;
+            }
+        `
         shadow.appendChild(style);
         shadow.appendChild(cloneTemplate);
     }
